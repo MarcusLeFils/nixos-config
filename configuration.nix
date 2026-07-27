9bf7dc015b22f8511bb6f5ccfdb719a613deb165
MarcusLeFils <marcus@gasdev.fr>
2026-07-27 19:07:49 +0200

    feat: Add helix

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,10 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    # Required by Gaspard
+    helix
+
+    # Your pacakges
     wget
     git
     fastfetch
